fa86ab945bc6d460b75d024a9baf778bb3f03b81
Ayush0115D <dhakreayush578@gmail.com>
2025-03-10 23:51:27 +0530

    added about files

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
+import Layout from './layout.jsx'
+import Home from './Home/home.jsx'
+import 
 const router=createBrowserRouter([
   {
     path:'/',
