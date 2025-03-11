8837f1e0764ef1675dc8ab741b0f0f8c74b7e01b
Ayush0115D <dhakreayush578@gmail.com>
2025-03-11 11:06:49 +0530

    updated few things in code

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Contact from './Contact/conatct.jsx'
+import Contact from './Contact/contact.jsx'
 import Layout from './layout.jsx'
 import Home from './Home/home.jsx'
 import Github,{githubInfoLoader} from './Github/github.jsx'
 import User from './User/user.jsx'
+import About from './components/about.jsx'
 
+// import About from './components/About/about.jsx'
 // const router = createBrowserRouter([
 //   {
 //     path: '/',
@@ -34,7 +36,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
-      {/* <Route path='about' element={<About />} /> */}
+      <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
       <Route 
